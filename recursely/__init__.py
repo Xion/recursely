7f7e9d7f9bde8249fc10a8397ec1e1830e15fe18
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2013-04-13 13:45:59 +0200

    setup.py & other scaffolding

---
+++ recursely/__init__.py
@@ -0,0 +1,6 @@
+"""
+recursely :: Recursive importer for Python submodules
+"""
+__version__ = "0.0.1"
+__author__ = "Karol Kuczmarski"
+__license__ = "BSD"
---
+++ setup.py
@@ -0,0 +1,34 @@
+"""
+recursely
+=========
+
+Recursive importer for Python submodules
+"""
+from setuptools import setup, find_packages
+
+import recursely
+
+
+setup(
+    name="recursely",
+    version=recursely.__version__,
+    description="Recursive importer for Python submodules",
+    long_description=__doc__,
+    author=recursely.__author__,
+    author_email="karol.kuczmarski@gmail.com",
+    url="http://github.com/Xion/recursely",
+    license="Simplified BSD",
+
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ],
+
+    platforms='any',
+    packages=find_packages(exclude=['tests']),
+)
