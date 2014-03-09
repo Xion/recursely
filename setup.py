b33de301339b361fc49c37079c72ea0d7021980b
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2014-03-09 22:04:58 +0000

    Use requirements-test.txt in setup.py

--- setup.py
+++ setup.py
@@ -22,9 +22,40 @@ that you want to import recurs(iv)ely.
 
 That's all to it, really.
 """
+import os
 from setuptools import setup, find_packages
 
 import recursely
+
+
+def read_requirements(filename='requirements.txt'):
+    """Reads the list of requirements from given file.
+
+    :param filename: Filename to read the requirements from.
+                     Uses ``'requirements.txt'`` by default.
+
+    :return: Requirements as list of strings
+    """
+    # allow for some leeway with the argument
+    if not filename.startswith('requirements'):
+        filename = 'requirements-' + filename
+    if not os.path.splitext(filename)[1]:
+        filename += '.txt'  # no extension, add default
+
+    def valid_line(line):
+        line = line.strip()
+        return line and not any(line.startswith(p) for p in ('#', '-'))
+
+    def extract_requirement(line):
+        egg_eq = '#egg='
+        if egg_eq in line:
+            _, requirement = line.split(egg_eq, 1)
+            return requirement
+        return line
+
+    with open(filename) as f:
+        lines = f.readlines()
+        return list(map(extract_requirement, filter(valid_line, lines)))
 
 
 setup(
@@ -49,5 +80,5 @@ setup(
 
     platforms='any',
     packages=find_packages(exclude=['tests']),
-    tests_require=['nose'],
+    tests_require=read_requirements('test'),
 )
