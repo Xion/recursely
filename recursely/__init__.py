b0bc25cccfd14b015dddca5168fe99f16f8468ec
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2014-03-13 00:00:12 +0000

    Bump version to 0.0.3

--- recursely/__init__.py
+++ recursely/__init__.py
@@ -1,7 +1,7 @@
 """
 recursely
 """
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __description__ = "Recursive importer for Python submodules"
 __author__ = "Karol Kuczmarski"
 __license__ = "Simplified BSD"
